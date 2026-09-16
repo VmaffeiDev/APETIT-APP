2645a1df456fb1df7a38e3ee0727b35fd6dd2dbc
Victor Maffei <vmaffei.dev@gmail.com>
2026-09-16 20:12:22 -0300

    feat: add database engine

---
+++ backend/app/db.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+
+from app.settings import settings
+
+
+engine: Engine = create_engine(settings.database_url, pool_pre_ping=True)
