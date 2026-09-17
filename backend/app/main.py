907f43abc60d5c32f865322a265f311ec7db7982
Victor Maffei <vmaffei.dev@gmail.com>
2026-09-17 08:04:02 -0300

    Register technical sheet API

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,7 @@ from app.api.meals import router as meals_router
 from app.api.menus import router as menus_router
 from app.api.prescriptions import router as prescriptions_router
 from app.api.profile import router as profile_router
+from app.api.technical_sheets import router as technical_sheets_router
 from app.settings import settings
 
 app = FastAPI(
@@ -32,6 +33,7 @@ app.include_router(menus_router)
 app.include_router(feedback_router)
 app.include_router(prescriptions_router)
 app.include_router(meals_router)
+app.include_router(technical_sheets_router)
 
 
 @app.get("/api/health", tags=["system"])
